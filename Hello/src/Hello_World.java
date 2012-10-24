9d63e57d6e7c5f196d256db1c8479c5905e3eb32
elman <elmo.parman@gmail.com>
2012-10-24 23:31:53 +0300

    little change 2

--- Hello/src/Hello_World.java
+++ Hello/src/Hello_World.java
@@ -5,8 +5,8 @@ public class Hello_World {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello  World");
-
+		System.out.println("Hello  World!");
+		System.out.println("Hello  again!");
 	}
 
 }
